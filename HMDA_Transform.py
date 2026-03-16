cbb80d9198f7000e03c04454637304a4626c565f
d-monkey-CG <138995840+d-monkey-CG@users.noreply.github.com>
2026-03-16 10:41:30 -0500

    Add files via upload
    This script transforms the datasets that were extracted from the HMDA website into a clean, combined format that will be loaded into Google cloud for use in BigQuery.

---
+++ HMDA_Transform.py
@@ -0,0 +1,74 @@
+import pandas as pd
+
+cols_needed_detail = [
+    "lei",
+    "action_taken",
+    "activity_year",
+    "census_tract",
+    "loan_amount",
+    "loan_to_value_ratio",
+    "interest_rate",
+    "property_value",
+    "income",
+    "debt_to_income_ratio",
+    "applicant_age",
+]
+df_1 = pd.read_csv(
+    "/Users/joannasanchez/Downloads/state_TX.csv",
+    usecols=cols_needed_detail,
+    low_memory=False,
+)
+df_2 = pd.read_csv(
+    "/Users/joannasanchez/Downloads/state_TX-2.csv",
+    usecols=cols_needed_detail,
+    low_memory=False,
+)
+df_3 = pd.read_csv(
+    "/Users/joannasanchez/Downloads/state_TX-3.csv",
+    usecols=cols_needed_detail,
+    low_memory=False,
+)
+df_4 = pd.read_csv(
+    "/Users/joannasanchez/Downloads/state_TX-4.csv",
+    usecols=cols_needed_detail,
+    low_memory=False,
+)
+df_5 = pd.read_csv(
+    "/Users/joannasanchez/Downloads/state_TX-5.csv",
+    usecols=cols_needed_detail,
+    low_memory=False,
+)
+
+df = pd.concat([df_1, df_2, df_3, df_4, df_5], ignore_index=True)
+
+# To change census_tract datatype
+df["census_tract"] = df["census_tract"].astype("Int64")
+
+# To change property_value datatype
+df["property_value"] = pd.to_numeric(df["property_value"], errors="coerce").astype(
+    "Int64"
+)
+
+# To change loan_to_value_ratio datatype
+df["loan_to_value_ratio"] = pd.to_numeric(
+    df["loan_to_value_ratio"], errors="coerce"
+).astype("Float64")
+
+# To change interest_rate datatype
+df["interest_rate"] = pd.to_numeric(df["interest_rate"], errors="coerce").astype(
+    "Float64"
+)
+
+# To change debt_to_income_ratio datatype
+df["debt_to_income_ratio"] = pd.to_numeric(
+    df["debt_to_income_ratio"], errors="coerce"
+).astype("Float64")
+
+# To change applicant_age datatype
+df["applicant_age"] = pd.to_numeric(df["applicant_age"], errors="coerce").astype(
+    "Int64"
+)
+
+df.info()
+
+df.to_parquet("/Users/joannasanchez/Downloads/HMDA_5yr_clean.parquet", index=False)
