f20f15f6b654be667293e1ffbce3f98bfeef6017
d-monkey-CG <138995840+d-monkey-CG@users.noreply.github.com>
2026-03-16 10:44:55 -0500

    Update file paths for CSV and Parquet files

--- HMDA_Transform.py
+++ HMDA_Transform.py
@@ -14,27 +14,27 @@ cols_needed_detail = [
     "applicant_age",
 ]
 df_1 = pd.read_csv(
-    "/Users/joannasanchez/Downloads/state_TX.csv",
+    "state_TX.csv",
     usecols=cols_needed_detail,
     low_memory=False,
 )
 df_2 = pd.read_csv(
-    "/Users/joannasanchez/Downloads/state_TX-2.csv",
+    "state_TX-2.csv",
     usecols=cols_needed_detail,
     low_memory=False,
 )
 df_3 = pd.read_csv(
-    "/Users/joannasanchez/Downloads/state_TX-3.csv",
+    "state_TX-3.csv",
     usecols=cols_needed_detail,
     low_memory=False,
 )
 df_4 = pd.read_csv(
-    "/Users/joannasanchez/Downloads/state_TX-4.csv",
+    "state_TX-4.csv",
     usecols=cols_needed_detail,
     low_memory=False,
 )
 df_5 = pd.read_csv(
-    "/Users/joannasanchez/Downloads/state_TX-5.csv",
+    "state_TX-5.csv",
     usecols=cols_needed_detail,
     low_memory=False,
 )
@@ -50,25 +50,17 @@ df["property_value"] = pd.to_numeric(df["property_value"], errors="coerce").asty
 )
 
 # To change loan_to_value_ratio datatype
-df["loan_to_value_ratio"] = pd.to_numeric(
-    df["loan_to_value_ratio"], errors="coerce"
-).astype("Float64")
+df["loan_to_value_ratio"] = pd.to_numeric(df["loan_to_value_ratio"], errors="coerce").astype("Float64")
 
 # To change interest_rate datatype
-df["interest_rate"] = pd.to_numeric(df["interest_rate"], errors="coerce").astype(
-    "Float64"
-)
+df["interest_rate"] = pd.to_numeric(df["interest_rate"], errors="coerce").astype("Float64")
 
 # To change debt_to_income_ratio datatype
-df["debt_to_income_ratio"] = pd.to_numeric(
-    df["debt_to_income_ratio"], errors="coerce"
-).astype("Float64")
+df["debt_to_income_ratio"] = pd.to_numeric(df["debt_to_income_ratio"], errors="coerce").astype("Float64")
 
 # To change applicant_age datatype
-df["applicant_age"] = pd.to_numeric(df["applicant_age"], errors="coerce").astype(
-    "Int64"
-)
+df["applicant_age"] = pd.to_numeric(df["applicant_age"], errors="coerce").astype("Int64")
 
 df.info()
 
-df.to_parquet("/Users/joannasanchez/Downloads/HMDA_5yr_clean.parquet", index=False)
+df.to_parquet("HMDA_5yr_clean.parquet", index=False)
